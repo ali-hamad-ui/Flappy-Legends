f5cb46e2d178186faf1c4e544ab9437b9a10977d
ali-hamad-ui <skybaseworld@gmail.com>
2026-07-17 00:18:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,12 +17,36 @@ let best = localStorage.getItem("best") || 0;
 
 bestScoreText.textContent = best;
 
+let coins=[];
+let coinScore=0;
+
+function spawnCoin(){
+
+coins.push({
+
+x:400,
+
+y:Math.random()*350+80
+
+});
+
+let coins=[];
+let coinScore=0;
+
+let clouds=[
+{x:60,y:90},
+{x:230,y:120},
+{x:350,y:70}
+];
+
 let bird;
 let pipes;
 let score;
 let gameRunning;
 
-function startGame(){
+function startGame(){coins=[];
+
+coinScore=0;}
 
 menu.style.display="none";
 
@@ -48,7 +72,11 @@ score=0;
 
 gameRunning=true;
 
-spawnPipe();
+spawnPipe();if(Math.random()<0.7){
+
+spawnCoin();
+
+}
 
 requestAnimationFrame(loop);
 
@@ -126,6 +154,10 @@ if(pipes.length==0||pipes[pipes.length-1].x<220){
 
 spawnPipe();
 
+spawnCoin();
+
+}
+
 }
 
 if(pipes[0].x<-70){
@@ -170,9 +202,102 @@ function loop(){
 
 if(!gameRunning)return;
 
-update();
+update();// Move clouds
 
-draw();
+clouds.forEach(c=>{
+
+c.x-=0.3;
+
+if(c.x<-70){
+
+c.x=450;
+
+}
+
+});
+
+draw();function draw(){
+
+// Sky
+const sky = ctx.createLinearGradient(0,0,0,600);
+sky.addColorStop(0,"#87CEEB");
+sky.addColorStop(1,"#3AA0FF");
+
+ctx.fillStyle = sky;
+ctx.fillRect(0,0,400,600);
+
+// Clouds
+ctx.fillStyle="white";
+
+clouds.forEach(c=>{
+ctx.beginPath();
+ctx.arc(c.x,c.y,25,0,Math.PI*2);
+ctx.arc(c.x+20,c.y-10,20,0,Math.PI*2);
+ctx.arc(c.x+40,c.y,25,0,Math.PI*2);
+ctx.fill();
+});
+
+// Pipes
+
+ctx.fillStyle="#28b463";
+
+pipes.forEach(pipe=>{
+
+ctx.fillRect(pipe.x,0,70,pipe.top);
+
+ctx.fillRect(pipe.x,pipe.bottom,70,600-pipe.bottom);
+
+});
+
+// Coins
+
+ctx.fillStyle="gold";
+
+coins.forEach(coin=>{
+
+ctx.beginPath();
+
+ctx.arc(coin.x,coin.y,10,0,Math.PI*2);
+
+ctx.fill();
+
+});
+
+// Ground
+
+ctx.fillStyle="#7CFC00";
+
+ctx.fillRect(0,570,400,30);
+
+// Bird
+
+ctx.save();
+
+ctx.translate(bird.x,bird.y);
+
+ctx.rotate(bird.velocity*0.05);
+
+ctx.fillStyle="yellow";
+
+ctx.beginPath();
+
+ctx.arc(0,0,18,0,Math.PI*2);
+
+ctx.fill();
+
+ctx.restore();
+
+// Score
+
+ctx.fillStyle="white";
+
+ctx.font="30px Arial";
+
+ctx.fillText("Score: "+score,20,40);
+
+ctx.fillText("Coins: "+coinScore,20,80);
+
+}
 
 requestAnimationFrame(loop);
 
